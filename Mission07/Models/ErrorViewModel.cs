fb7d14466ff8e2219a618b741310c92d96cf3770
Jacob Poor <jacob.poor98@gmail.com>
2022-02-16 13:05:18 -0700

    Basic

---
+++ Mission07/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Mission07.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
